1916b3276c158aedec73e4df659d30d1743bf84d
Saleh Bagheri <salehbagheri1381@gmail.com>
2024-07-28 16:46:29 +0330

    feat: ai spawner

---
+++ Assets/Scripts/AISpawner.cs
@@ -0,0 +1,63 @@
+using Feature.SceneLoader;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AISpawner : MonoBehaviour
+{
+    [SerializeField] private AIController[] aiControllers;
+    [SerializeField] private int aliveAI = 10;
+    [SerializeField] private float minRespawnDelay = 0.5f;
+    [SerializeField] private float maxRespawnDelay = 1f;
+
+    private List<AIController> _currentAIs = new();
+    private List<AIController> _aliveAIs = new();
+    private PlayerSpawnPoint[] _spawnPoints;
+
+    private void Start()
+    {
+        SceneController.Instance.OnSceneLoaded += Init;
+    }
+
+    private void OnDestroy()
+    {
+        SceneController.Instance.OnSceneLoaded -= Init;
+    }
+
+    private void Init()
+    {
+        _spawnPoints = FindObjectsOfType<PlayerSpawnPoint>();
+
+        if (aiControllers.Length == 0)
+            return;
+
+        for (int i = 0; i < aliveAI; i++)
+            SpawnAI();
+    }
+
+    private void SpawnAI()
+    {
+        var ai = aiControllers[Random.Range(0, aiControllers.Length)];
+        var availablePoints = _spawnPoints.Where(x => x.PlayerInArea() == false && x.IsPointInCameraSight() == false).ToList();
+        var spawnPoint = availablePoints[Random.Range(0, availablePoints.Count)];
+        var instance = Instantiate(ai, spawnPoint.transform.position, Quaternion.identity, null);
+
+        _currentAIs.Add(instance);
+        _aliveAIs.Add(instance);
+    }
+
+    public void RespawnAI(AIController aiController)
+    {
+        _aliveAIs.Remove(aiController);
+        StartCoroutine(RespawnAICoroutine(aiController));
+    }
+
+    private IEnumerator RespawnAICoroutine(AIController aiController)
+    {
+        yield return new WaitForSeconds(Random.Range(minRespawnDelay, maxRespawnDelay));
+
+        // TODO: respawn fuckin ai
+        _aliveAIs.Add(aiController);
+    }
+}
--- Assets/Scripts/PlayerSpawnPoint.cs
+++ Assets/Scripts/PlayerSpawnPoint.cs
@@ -1,10 +1,38 @@
+using Logic.Player;
 using UnityEngine;
 
 public class PlayerSpawnPoint : MonoBehaviour
 {
+    [SerializeField] private float playerCheckRadius = 2f;
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawLine(transform.position - Vector3.up, transform.position + Vector3.up);
+    }
+
+    public bool PlayerInArea()
+    {
+        var colliders = Physics2D.OverlapCircleAll(transform.position, playerCheckRadius);
+
+        foreach (var collider in colliders)
+        {
+            if (collider.TryGetComponent<Player>(out var result))
+                return true;
+        }
+
+        return false;
+    }
+
+    public bool IsPointInCameraSight()
+    {
+        var cam = Camera.main;
+
+        var directionToObject = transform.position - cam.transform.position;
+
+        if (Vector3.Angle(cam.transform.forward, directionToObject) > cam.fieldOfView)
+            return false;
+
+        return true;
     }
 }
